b4f4e2222b0e89c3ef02d47b7ccb44c51fa7bd6e
ahnanhya <24sbca005@psgrkcw.ac.in>
2026-07-14 21:22:21 +0530

    new file:   lib/mysql-connector-j-8.0.33.jar 	modified:   src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,76 +1,47 @@
+import java.awt.Desktop;
+import java.io.File;
 import java.util.ArrayList;
 
 public class Main {
+    private static Database database;
+    private static ArrayList<Movie> movieList;
     public static void main(String[] args) {
         // Connect to Database
-        Database database = new Database();
+        database = new Database();
         // Load Movies
-        ArrayList<Movie> movies = database.getMovies();
-        System.out.println("--------------------------------");
-        System.out.println("Movie Recommendation System");
-        System.out.println("--------------------------------");
-        System.out.println("Movies Loaded : " + movies.size());
+        movieList = database.getMovies();
         // Sort Movies by Rating
-        if (movies.size() > 1) {
-            DataStructures.mergeSort(movies, 0, movies.size() - 1);
-        }
-        System.out.println("\nTop Rated Movies");
-        for (Movie movie : movies) {
-            System.out.println(
-                    movie.getTitle() +
-                    " | " +
-                    movie.getGenre() +
-                    " | Rating : " +
-                    movie.getRating()
-            );
+        if (!movieList.isEmpty()) {
+            DataStructures.mergeSort(movieList, 0, movieList.size() - 1);
         }
         // Create Genre HashMap
-        DataStructures.createGenreMap(movies);
-        // ----------------------------
-        // Binary Search Demo
-        // ----------------------------
-        System.out.println("\nSearching for 'Leo'...");
-        Movie searchedMovie =
-                RecommendationEngine.searchMovie(movies, "Leo");
-        if (searchedMovie != null) {
-            System.out.println("Movie Found : "
-                    + searchedMovie.getTitle());
-            // Recommendations
-            System.out.println("\nRecommended Movies");
-            ArrayList<Movie> recommendations =
-                    RecommendationEngine.getTopRecommendations(
-                            searchedMovie,
-                            5
-                    );
-            for (Movie movie : recommendations) {
-                System.out.println(movie.getTitle()
-                        + " (" + movie.getRating() + ")");
+        DataStructures.createGenreMap(movieList);
+        System.out.println("Movie Recommendation System Started");
+        System.out.println("Movies Loaded : " + movieList.size());
+        // Open Frontend
+        openHomePage();
+    }
+    // Opens index.html
+    private static void openHomePage() {
+        try {
+            File file = new File("frontend/index.html");
+            if (file.exists()) {
+                Desktop.getDesktop().browse(file.toURI());
+            } else {
+                System.out.println("Cannot find frontend/index.html");
+                System.out.println(file.getAbsolutePath());
             }
-            // Wishlist Demo
-            RecommendationEngine.addToWishlist(searchedMovie);
-            // Watched Demo
-            RecommendationEngine.addToWatched(searchedMovie);
         }
-        else {
-            System.out.println("Movie Not Found!");
+        catch (Exception e) {
+            e.printStackTrace();
         }
-        // ----------------------------
-        // Display Wishlist
-        // ----------------------------
-        System.out.println("\nWishlist");
-        for (Movie movie :
-                RecommendationEngine.getWishlist()) {
-            System.out.println(movie.getTitle());
-        }
-        // ----------------------------
-        // Display Watched Movies
-        // ----------------------------
-        System.out.println("\nRecently Watched");
-        for (Movie movie :
-                RecommendationEngine.getWatchedMovies()) {
-            System.out.println(movie.getTitle());
-        }
-        // Close Database
-        database.closeConnection();
+    }
+    // Returns all movies
+    public static ArrayList<Movie> getMovieList() {
+        return movieList;
+    }
+    // Returns database object
+    public static Database getDatabase() {
+        return database;
     }
 }
